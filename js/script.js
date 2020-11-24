5c92b0f54ee44ec3a6c309503d148287188b8f11
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-11-24 15:31:19 +0100

    Avvio progetto

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Per facilitarvi la vita usate immagini delle stesse dimensioni :)
+// Lo slider prevederà due frecce (icone) che permettono di scorrere tra le immagini dello slider.
+// Inoltre per scorrere le immagini permettere anche l’uso delle frecce sinistra e destra della tastiera.
+// Utiliziamo una classe first e last per capire quali sono la prima e ultima immagine dello slider.
+// Utilizziamo una classe active per aiutarci a capire quale è l’immagine attuale da visualizzare nello slider.
+// Bonus:
+// Clicchiamo sui pallini e mostriamo l’immagine corrispondente.
+// Generiamo i pallini con JS.
+
+$(document).ready(
+  function() {
+
+  };
+);
