3b2f3d13a63dbd5121c6fc580f47536fe8a7fc1d
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-11-24 19:35:42 +0100

    Diversi FIX
    - Non avevo collegato JQuery
    - Non avevo inserito la funzione anonima dopo il click
    - Non avevo messo le virgolette per specificare cosa prendere dopo il documet ready

--- js/script.js
+++ js/script.js
@@ -10,26 +10,34 @@ $(document).ready(
   function() {
 
     // Questo accade quando clicco sull freccia a sinistra
-    $(.previous).click(
-      previousItem();
+    $(".previous").click(
+      function() {
+        previousItem();
+      }
     );
 
     // Questo accade quando clicco sull freccia a destra
-    $(.next).click(
-      nextItem();
+    $(".next").click(
+      function() {
+        nextItem();
+      }
     );
-  };
+    
+  }
 );
 
 // FUNZIONI
 function previousItem() {
-  // 1 - Devo selezionare gli elementi interessati
-  var activeImage = $(".images_container");
-  var activeCircle = $("#nav ul li");
+  // 1 - Devo selezionare gli elementi interessati con le variabili
+  var activeImage = $("div.images_container div.active");
+  var activeCircle = $("#nav ul li.active_circle");
 
-  // 2 - Togliere la classe active
-
-  // 3 - Assegnare la classe active
+  // 2 - Togliere la classe active all'elemento selezionato
+  activeImage.removeClass("active");
+  // 3 - Assegnare la classe active all'elemento subito dopo
+  activeImage.next().addClass("active");
 }
 
-function nextItem() {}
+function nextItem() {
+
+}
