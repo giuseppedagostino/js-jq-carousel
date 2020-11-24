888996c1be5c9f7278bdfca1419f127ecf5b4c97
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-11-24 22:24:04 +0100

    Scorrimento slider con le freccette della tastiera

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-// Lo slider prevederà due frecce (icone) che permettono di scorrere tra le immagini dello slider.
-// Inoltre per scorrere le immagini permettere anche l’uso delle frecce sinistra e destra della tastiera.
-// Utiliziamo una classe first e last per capire quali sono la prima e ultima immagine dello slider.
-// Utilizziamo una classe active per aiutarci a capire quale è l’immagine attuale da visualizzare nello slider.
 // Bonus:
 // Clicchiamo sui pallini e mostriamo l’immagine corrispondente.
 // Generiamo i pallini con JS.
@@ -20,6 +16,19 @@ $(document).ready(
     $(".next").click(
       function() {
         nextItem();
+      }
+    );
+
+    $(document).keydown(
+      function() {
+
+        // Utilizzo le freccette della tastiera per passare da un'immagine all'altra
+        if (event.keyCode == 37) {
+          previousItem();
+        } else if (event.keyCode == 39) {
+          nextItem();
+        }
+        
       }
     );
 
